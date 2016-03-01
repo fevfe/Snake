cbb61d315113bdf3a1088e8b434ff1c703665ab7
fevfe <fevfe@mail.ru>
2016-03-01 22:57:08 +0200

    Осваиваю класс "Список"

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -16,6 +16,39 @@ namespace MySnake
             Point p2 = new Point(4, 7, '#' );
             p2.Draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+            numList.Reverse();
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            Point p3 = new Point(5, 8, '?');
+            Point p4 = new Point(6, 9, '!');
+            pList.Add(p3);
+            pList.Add(p4);
+            p1.x = 7;
+            p1.y = 10;
+
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+
             Console.ReadLine();
         }
     }
